0efdb8dfdd718e140dda5c1d29ccf6e9fa6281ba
GarryVar <darisvet.sokolov@yandex.ru>
2023-06-07 23:07:10 +0300

    Added search component to header and remove logo

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import s from "./Header.module.css";
-import Logo from "../Logo/Logo";
+import SearchForm from "../SearchForm/SearchForm";
+// import Logo from "../Logo/Logo";
 
 const Header = (props) => {
     return (
         <header className={s.header}>
             <div className={s.layout}>
-                <Logo logoState={props.logoState}/>
+                <SearchForm />
+                <p className={s.slogan}>Cactuchat</p>
+                {/*<Logo logoState={props.logoState}/>*/}
             </div>
         </header>
     )
